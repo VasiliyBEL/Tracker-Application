1dac90a7bb52a2f317834fe5c9953fa38cf6540b
VasiliyBEL <vaspn001@gmail.com>
2022-11-04 22:50:30 +0300

    added Sidebar, Dashboard controller and view.

---
+++ Tracker Application/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tracker_Application.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
