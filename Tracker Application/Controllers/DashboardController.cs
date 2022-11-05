00701da26e3e47d9a272cee9bdb4bc22ec60bf2e
VasiliyBEL <vaspn001@gmail.com>
2022-11-05 18:54:10 +0300

    Donut !!!

--- Tracker Application/Controllers/DashboardController.cs
+++ Tracker Application/Controllers/DashboardController.cs
@@ -43,6 +43,18 @@ namespace Tracker_Application.Controllers
             culture.NumberFormat.CurrencyNegativePattern = 1;
             ViewBag.Balance = String.Format(culture, "{0:C0}", Balance);
 
+            //Doughnut Chart - Expense By Category 
+            ViewBag.DoughnutChartData = SelectedTransactions
+                .Where(i => i.Category.Type == "Expense")
+                .GroupBy(j => j.Category.CategoryId)
+                .Select(k => new
+                {
+                    categoryTitleWithIcon = k.First().Category.Icon+" "+ k.First().Category.Title,
+                    amount = k.Sum(j => j.Amount),
+                    formattedAmount = k.Sum(j => j.Amount).ToString("C0"),
+                })
+                .ToList();
+
             return View();
         }
     }
