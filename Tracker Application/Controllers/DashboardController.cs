3ce5b74f857ce93f0723e2badc87a84da664650a
VasiliyBEL <vaspn001@gmail.com>
2022-11-05 17:25:44 +0300

    Modified DashboardCategory

--- Tracker Application/Controllers/DashboardController.cs
+++ Tracker Application/Controllers/DashboardController.cs
@@ -1,11 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tracker_Application.Models;
 
 namespace Tracker_Application.Controllers
 {
     public class DashboardController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public DashboardController(ApplicationDbContext context)
         {
+            _context = context;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            //Last 7 Days
+            DateTime StartDate = DateTime.Today.AddDays(-6);
+            DateTime EndDate = DateTime.Today;
+
+            List<Transaction> SelectedTransactions = await _context.Transactions
+                .Include(x => x.Category)
+                .Where(y => y.Date >= StartDate && y.Date <= EndDate)
+                .ToListAsync();
+
+            //Total Income
+            int TotalIncome = SelectedTransactions
+                .Where(i => i.Category.Type == "Income")
+                .Sum(j => j.Amount);
+            ViewBag.TotalIncome = TotalIncome.ToString("C0");
+
+            //Total Expense
+            int TotalExpense= SelectedTransactions
+                .Where(i => i.Category.Type == "Expense")
+                .Sum(j => j.Amount);
+            ViewBag.TotalIncome = TotalIncome.ToString("C0");
+
+            //Balance
+            int Balance = TotalIncome - TotalExpense;
+            ViewBag.Balance = Balance.ToString("C0");
+
             return View();
         }
     }
